eb84fad46e7a23155e1770ba9ff911b6402db6ef
William Miles <willmiles@live.co.uk>
2021-06-28 16:42:53 +0100

    contact number change

--- src/Components/ContactPage.js
+++ src/Components/ContactPage.js
@@ -6,7 +6,7 @@ const ContactPage = () => {
       <h2 className="orange-text title">Contact</h2>
       <h3>Becky Miles</h3>
       <h4>becky.miles@rapportbydesign.co.uk</h4>
-      <h4>07519 682215</h4>
+      <h4>07984 293847</h4>
     </div>
   );
 };
